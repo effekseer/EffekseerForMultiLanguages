47a8308aa8578e4cac62c4f92a5302180c29aeb8
durswd <swda.durl@gmail.com>
2020-04-29 12:57:20 +0900

    Add workflow

--- examples/libGtxSample.bat
+++ examples/libGtxSample.bat
@@ -3,7 +3,7 @@ cd ..
 mkdir build
 cd build
 
-cmake ..
+cmake -D BUILD_DX12=OFF ..
 cmake --build . --config Release
 
 copy src\cpp\Release\EffekseerNativeForJava.dll ..\examples\libGdxSample\core\assets\EffekseerNativeForJava.dll
