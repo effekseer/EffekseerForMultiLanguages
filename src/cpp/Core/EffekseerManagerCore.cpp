45ab45513e05803537c07f4cf5d59887d17e4942
Riccardo Balbo <riccardo@forkforge.net>
2020-09-13 21:06:35 +0200

    Add Exists function

--- src/cpp/Core/EffekseerManagerCore.cpp
+++ src/cpp/Core/EffekseerManagerCore.cpp
@@ -189,6 +189,10 @@ void EffekseerManagerCore::DrawFront()
 	renderer_->EndRendering();
 }
 
+bool EffekseerManagerCore::Exists(int handle)
+{
+	return manager_->Exists(handle);
+}
 
 
 void EffekseerManagerCore::SetProjectionMatrix(float v0,float v1,float v2,float v3,float v4,float v5,float v6,float v7,float v8,float v9,float v10,float v11,float v12,float v13,float v14,float v15)
--- src/cpp/Core/EffekseerManagerCore.h
+++ src/cpp/Core/EffekseerManagerCore.h
@@ -42,6 +42,7 @@ public:
 	
 	void SetCameraMatrix(float v0,float v1,float v2,float v3,float v4,float v5,float v6,float v7,float v8,float v9,float v10,float v11,float v12,float v13,float v14,float v15);
 	
+	bool Exists(int handle);
 
 	void SetViewProjectionMatrixWithSimpleWindow(int32_t windowWidth, int32_t windowHeight);
 
