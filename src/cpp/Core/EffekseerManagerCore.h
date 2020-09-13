36609af11fe86fae75fec7319f4a98dbcad13526
Riccardo Balbo <riccardo@forkforge.net>
2020-09-13 10:43:13 +0200

    Add some missing methods

--- src/cpp/Core/EffekseerManagerCore.h
+++ src/cpp/Core/EffekseerManagerCore.h
@@ -26,11 +26,22 @@ public:
 
 	int Play(EffekseerEffectCore* effect);
 
+	void SetPaused(int handle,bool v);
+
+	void SetShown(int handle,bool v);
+
 	void SetEffectPosition(int handle, float x, float y, float z);
+
+	void SetEffectTransformMatrix(int handle,float v0,float v1,float v2,float v3,float v4,float v5,float v6,float v7,float v8,float v9,float v10,float v11);
 
 	void DrawBack();
 
 	void DrawFront();
+
+	void SetProjectionMatrix(float v0,float v1,float v2,float v3,float v4,float v5,float v6,float v7,float v8,float v9,float v10,float v11,float v12,float v13,float v14,float v15);
+	
+	void SetCameraMatrix(float v0,float v1,float v2,float v3,float v4,float v5,float v6,float v7,float v8,float v9,float v10,float v11,float v12,float v13,float v14,float v15);
+	
 
 	void SetViewProjectionMatrixWithSimpleWindow(int32_t windowWidth, int32_t windowHeight);
 };
