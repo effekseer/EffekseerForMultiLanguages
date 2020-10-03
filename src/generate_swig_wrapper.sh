d388811dfc3be17efc60b52161784aedf33f0ed5
Riccardo Balbo <riccardo@forkforge.net>
2020-10-03 12:14:41 +0200

    Exclude generated

--- src/generate_swig_wrapper.sh
+++ src/generate_swig_wrapper.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
+mkdir -p java/swig
+mkdir -p csharp/swig
+mkdir -p python/swig
+
+mkdir -p cpp/java
+mkdir -p cpp/csharp
+mkdir -p cpp/python
+
 swig -c++ -java -package Effekseer.swig -o cpp/java/dll.cpp -outdir java/swig/ EffekseerCore.i
 swig -c++ -csharp -namespace Effekseer.swig -o cpp/csharp/dll.cpp -outdir csharp/swig/ -dllimport EffekseerNativeForCSharp EffekseerCore.i
 swig -c++ -python -o cpp/python/dll.cpp -outdir python/swig/ EffekseerCore.i
